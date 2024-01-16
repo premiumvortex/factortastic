59c5cfc20c9575a07997de05269d3dd644824223
Faraz <gkfaraz@berkeley.edu>
2024-01-16 03:24:47 -0800

    [Faraz]: Added material-ui to docker

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Alert from "./components/Alert.jsx";
 
 function App() {
   const [count, setCount] = useState(0)
