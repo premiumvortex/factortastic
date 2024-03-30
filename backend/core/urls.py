0f0b14dd0f696598c3af452f86141cf90f06fd1e
Faraz <gkfaraz@berkeley.edu>
2024-03-29 19:44:34 -0700

    FACTOR-38 [Faraz]: Added hellowrod page to backend

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -4,6 +4,4 @@ from django.contrib import admin
 
 urlpatterns = [
     path('hello/', hello_view, name='hello'),
-    path('admin/', admin.site.urls),
-    path('', include('core.urls')),
 ]
--- backend/core/views.py
+++ backend/core/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def hello_view(request):
-    return HttpResponse('Hello from backend')
+    return HttpResponse('Hello from backend. Faraz was here!')
