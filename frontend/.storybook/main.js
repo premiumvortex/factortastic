c55b1b96e472fa12280964c52970400927ab120b
Anna Aksenov <annaaksenov8@gmail.com>
2024-04-08 16:31:24 -0700

    FACTOR-49 [Anna]: Created layout for settings page. Buttons can toggle and hover + back button.

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -1,3 +1,25 @@
+// /** @type { import('@storybook/react-vite').StorybookConfig } */
+// const config = {
+//   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+//   addons: [
+//     "@storybook/addon-links",
+//     "@storybook/addon-essentials",
+//     "@storybook/addon-onboarding",
+//     "@storybook/addon-interactions",
+//   ],
+//   framework: {
+//     name: "@storybook/react-vite",
+//     options: {},
+//   },
+//   docs: {
+//     autodocs: "tag",
+//   },
+// };
+
+// export default config;
+
+import { mergeConfig } from 'vite';
+
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
@@ -14,5 +36,19 @@ const config = {
   docs: {
     autodocs: "tag",
   },
+  // Add the Vite specific configuration here
+  async viteFinal(config, { configType }) {
+    // Return the customized Vite config
+    return mergeConfig(config, {
+      server: {
+        watch: {
+          // Use polling if necessary, especially useful in Docker environments
+          usePolling: true,
+          interval: 100, // Check for changes every 100ms
+        },
+      },
+    });
+  },
 };
+
 export default config;
---
+++ frontend/src/components/settings/Settings.stories.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Settings from './Settings.jsx';
+
+export default {
+  title: 'Settings',
+  component: Settings,
+};
+
+export const DefaultSettings = () => (
+  <Settings/>
+);
