faa00ead4fed8b48cbf28a50ef24de0fdcce3740
Anna Aksenov <annaaksenov8@gmail.com>
2024-04-16 10:19:04 -0700

    FACTOR-49 [Anna]: Completed layout and css for Settings, KeyPage, and ContactUsPage.

--- frontend/src/components/Settings/Settings.jsx
+++ frontend/src/components/Settings/Settings.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { SoundProvider } from '../Sound/SoundContext';
 import SoundComponent from '../Sound/SoundComponent';
 import SoundSettings from "../Sound/SoundSettings"; // Import from second component
@@ -6,49 +6,54 @@ import SettingButton from '../button/SettingButton';
 import MusicToggle from '../button/MusicToggle';
 import SoundFxToggle from '../button/SoundFxToggle';
 import BackButton from "../button/BackButton";
+import '../Settings/settings.css';
 // import KeyPage from '../../page/KeyPage';
 // import ContactUsPage from '../../page/ContactUsPage';
-import '../Settings/settings.css';
 
-// Add in KeyPage and ContactUsPage components
 function CombinedSettings() {
+    const [showPopup, setShowPopup] = useState(true);
+    const handleClosePopup = () => {
+        setShowPopup(false);
+    };
 return (
     <>
-        <SoundProvider>
-            <div className="App">
-                <SoundComponent />
-                <SoundSettings />  {/* Integrated SoundSettings from the second component */}
-                <div>
-                    <BackButton />
-                </div>
-                <div className='setting-content'>
-                    <div className='setting-title padding-bottom'>
-                        <h2>Game</h2>
-                        <h2 className='pink'>Settings</h2>
+     <div className={`popup-container ${showPopup ? '' : 'hide'}`}>
+            <SoundProvider>
+                <div className="App">
+                    <SoundComponent />
+                    <SoundSettings />  {/* Integrated SoundSettings from the second component */}
+                    <div>
+                        <BackButton onClick={handleClosePopup} />
                     </div>
-                    <div className='row justify-center padding-bottom'>
-                        <div className='column-half sound-labels'>
-                            <h2>Music:</h2>
-                            <h2>SoundFx:</h2>
+                    <div className='setting-content'>
+                        <div className='setting-title padding-bottom'>
+                            <h2>Game</h2>
+                            <h2 className='pink'>Settings</h2>
                         </div>
-                        <div className='column-half flex flex-column space-evenly'>
-                            <MusicToggle/>
-                            <SoundFxToggle/>
+                        <div className='row justify-center padding-bottom'>
+                            <div className='column-half sound-labels'>
+                                <h2>Music:</h2>
+                                <h2>SoundFx:</h2>
+                            </div>
+                            <div className='column-half flex-column space-evenly padding-left-30'>
+                                <MusicToggle />
+                                <SoundFxToggle />
+                            </div>
                         </div>
-                      </div>
-                    <div className='row justify-center'>
-                        <div className='column-half flex flex-column'>
-                            <SettingButton text={'Key'} />
-                            <SettingButton text={'Menu'} />
-                        </div>
-                        <div className='column-half flex flex-column'>
-                            <SettingButton text={'Privacy Policy'} />
-                            <SettingButton text={'Contact Us'} />
+                        <div className='row justify-center'>
+                            <div className='column-half flex flex-column'>
+                                <SettingButton text={'Key'} />
+                                <SettingButton text={'Menu'} />
+                            </div>
+                            <div className='column-half flex flex-column'>
+                                <SettingButton text={'Privacy Policy'} />
+                                <SettingButton text={'Contact Us'} />
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </SoundProvider>
+            </SoundProvider>
+    </div>
     </>
     );
 }
--- frontend/src/components/popup/PopUp.jsx
+++ frontend/src/components/popup/PopUp.jsx
@@ -1,12 +1,8 @@
 import React from 'react';
-import Settings from '../settings/Settings';
-import KeyPage from '../../page/KeyPage';
 
 export default function PopUp() {
   return (
     <>
-    {/* <Settings/> */}
-    {/* <KeyPage/> */}
     </>
   );
 }
