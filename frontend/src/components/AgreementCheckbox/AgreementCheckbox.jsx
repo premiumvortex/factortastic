fb27520ccc009e65bb9d3c84f68441ffbc6d785e
Mayuri Buckius <mbuckius@uci.edu>
2024-04-30 22:19:51 -0700

    FACTOR-47 created AgreementCheckbox component

---
+++ frontend/src/components/AgreementCheckbox/AgreementCheckbox.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Checkbox from '@mui/material/Checkbox';
+import PropTypes from 'prop-types';
+
+import '../../index.css';
+
+export const AgreementCheckbox = ({ id, isChecked, toggleCheckbox }) => {
+    return (
+        <Checkbox
+            id={id}
+            checked={isChecked}
+            onChange={toggleCheckbox}
+       
+            sx={{
+                color: 'black',
+                '&.Mui-checked': { color: 'var(--cyan)' },                        
+                '& .MuiSvgIcon-root': { fontSize: '48px' }
+            }}
+        />
+    );
+};
+
+AgreementCheckbox.propTypes = {
+    id: PropTypes.string.isRequired,
+    isChecked: PropTypes.bool.isRequired,
+    toggleCheckbox: PropTypes.func.isRequired,
+};
