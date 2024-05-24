0f1651687786fa2ba4457d2abbc5442b3129a0a6
Mayuri Buckius <mbuckius@uci.edu>
2024-05-23 17:51:13 -0700

    FACTOR-47 move styles into jsx file

--- frontend/src/components/HomePage/HomePageComponents/HomePageTitle/HomePageTitle.jsx
+++ frontend/src/components/HomePage/HomePageComponents/HomePageTitle/HomePageTitle.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 
-import './HomePageTitle.css';
 import '../../../../index.css';
 
 export const HomePageTitle = () => {
     return (
-        <h1 className="home-page-title">
+        <h1 className="home-page-title" style={{textTransform: 'uppercase'}}>
             <span className="page-title" style={{color: 'var(--cyan)'}}>factor</span>
             <span className="page-title" style={{color: 'var(--pink)'}}>tastic!</span>
         </h1>
