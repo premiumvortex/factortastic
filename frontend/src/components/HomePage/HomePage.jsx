f916ce26b86aebd38b588073b5af8d603e88526f
Mayuri Buckius <mbuckius@uci.edu>
2024-05-27 11:18:22 -0700

    FACTOR-47 create CharactersBackgroundImage component

--- frontend/src/components/HomePage/HomePage.jsx
+++ frontend/src/components/HomePage/HomePage.jsx
@@ -1,30 +1,33 @@
 import React, { useState } from 'react';
 
+import { Agreement } from './HomePageComponents/Agreement/Agreement';
+import { CharactersBackgroundImage } from './HomePageComponents/CharactersBackgroundImage/CharactersBackgroundImage';
 import { HomePageTitle } from './HomePageComponents/HomePageTitle/HomePageTitle';
 import { StartButton } from './HomePageComponents/StartButton/StartButton';
-import { Agreement } from './HomePageComponents/Agreement/Agreement';
 
 import './HomePage.css';
 import '../../index.css';
 
 // HomePage component displays title and a start button
+// need to add setting icon button
 export const HomePage = () => {
     const [showAgreement, setShowAgreement] = useState(false);
 
-    const handleClick = () => {
+    const handleStartButtonClick = () => {
         setShowAgreement(true);
     };
 
-    const handleClosePopup = () => {
+    const handleCloseAgreement = () => {
         setShowAgreement(false);
     };
 
     return (
         <div className="home-page">
             <HomePageTitle />
-            <StartButton handleClick={handleClick}/>
-
-            {showAgreement && <Agreement onClose={handleClosePopup} />}      
+            <StartButton handleClick={handleStartButtonClick}/>
+            <div className="background-image"><CharactersBackgroundImage /></div>
+            
+            {showAgreement && <Agreement onClose={handleCloseAgreement} />}      
         </div>
     );
 }
---
+++ frontend/src/components/HomePage/HomePageComponents/CharactersBackgroundImage/CharactersBackgroundImage.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import { CharacterImage } from '../../../CharacterImage/CharacterImage';
+
+import './CharactersBackgroundImage.css';
+// import './test.css';
+
+// list of character strings to loop through
+const characters = ['shoes','mice','leaf','star','cans','rainbow','octopus','fox','toes','frogs','eggs'];
+
+// Characters component displays the background image of the HomePage
+// Displays each character and positions them as needed
+export const CharactersBackgroundImage = () => {
+    return (
+        <div className='photo-container'>
+            {characters.map(character => (
+                <div key={character} className={`${character}-container`}>
+                    <CharacterImage character={character} />
+                </div>
+            ))}
+        </div>
+    );
+};
+
