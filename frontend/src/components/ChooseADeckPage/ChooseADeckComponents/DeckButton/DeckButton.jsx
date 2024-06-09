aa97834273e369973bbf11224f6ce04892a309c4
Mayuri Buckius <mbuckius@uci.edu>
2024-06-08 20:37:57 -0700

    FACTOR-45 show background image depending on status

--- frontend/src/components/ChooseADeckPage/ChooseADeckComponents/DeckButton/DeckButton.jsx
+++ frontend/src/components/ChooseADeckPage/ChooseADeckComponents/DeckButton/DeckButton.jsx
@@ -1,22 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Lock from './Lock.svg'; 
+import LockIcon from '@mui/icons-material/Lock'; 
 
 import './DeckButton.css';
 import '../../../../index.css';
 
-export const DeckButton = ({ deckName, status, color, onClick }) => {
-    // Adjust styling based on color and status
-    const buttonStyle = {
-        backgroundColor: color,
-        color: color,
-        pointerEvents: status === 'disabled' ? 'none' : 'auto'
-        // opacity: unlocked === 'disabled' ? 0.5 : 1, // Optionally, make disabled buttons appear faded
-    };
+export const DeckButton = ({ status, color, onClick }) => {
 
-    // Determine content: Show deckName for 'unlocked' and 'disabled', lock icon for 'locked'
-    const buttonContent = status === 'locked' ? <img className="lock-icon" src={Lock} alt="Locked Icon"/> : deckName;
-
+    
     // Only call onClick() if button is enabled
     const handleClick = () => {
         if (status !== 'disabled') {
@@ -29,15 +20,15 @@ export const DeckButton = ({ deckName, status, color, onClick }) => {
             className="deck-button" 
             aria-disabled={status === 'disabled'} 
             onClick={handleClick} 
-            style={buttonStyle}
+            style={{'--color': color }}
         >
-            <p className="button-name secondary">{buttonContent}</p>
+            {status === 'locked' && <LockIcon sx={{ color: color, fontSize: '7vw' }} />}
+            {status === 'disabled' && <p className="secondary">Coming Soon</p>}
         </button>
     );
 };
 
 DeckButton.propTypes = {
-    deckName: PropTypes.string.isRequired,
     status: PropTypes.string.isRequired,
     color: PropTypes.string.isRequired,
     onClick: PropTypes.func.isRequired,
