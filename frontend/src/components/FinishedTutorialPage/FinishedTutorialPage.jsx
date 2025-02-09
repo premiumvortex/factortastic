0bb45c3b5569cf25417ba4c123930092dcdeb5a0
Anna Aksenov <annaaksenov8@gmail.com>
2025-02-09 15:01:51 -0800

    FACTOR-64 [Anna]: Completed Finished Tutorial Page component.

--- frontend/src/components/FinishedTutorialPage/FinishedTutorialPage.jsx
+++ frontend/src/components/FinishedTutorialPage/FinishedTutorialPage.jsx
@@ -2,27 +2,38 @@ import React from "react";
 import "../../index.css";
 import { SettingLongButton } from "../SettingLongButton/SettingLongButton";
 
-export default function FinishedTutorialPage() {
 
+export default function FinishedTutorialPage() {
   return (
     <div style={{
       display: 'flex',
       flexDirection: 'column',
-      alignContent: 'center', //ensures items are centered horiz.
-      justifyContent: 'space-evenly',  //distributes items evenly
-      height: '100vh',
-      color: 'white',
-      textAlign: 'center'
+      justifyContent: 'space-evenly',
+      alignItems: 'center',
+      minHeight: '100vh',
+      padding: '5vh 0',
+      textAlign: 'center',
+      color: 'white'
+    }}>
+      <div style={{ width: '100%' }}>
+        <span style={{ color: 'var(--pink)', fontSize: '4vw' }} className="page-title">Let's </span>
+        <span style={{ color: 'var(--cyan)', fontSize: '4vw' }} className="page-title">play</span>
+      </div>
+      <div style={{ width: '100%' }}>
+        <span style={{ fontSize: '4vw' }} className="page-title">You're ready for our adventure!</span>
+      </div>
+
+      <div style={{
+        display: 'flex',
+        justifyContent: 'center',
+        minWidth: '50%',
       }}>
-      <div style={{ padding: '5vh 0', fontSize: '5vh' }}>Let's play</div>
-      <div style={{ padding: '5vh 0', fontSize: '5vh' }}>You're ready for our adventure!</div>
-      <div style={{}}>
         <SettingLongButton
-        text="Continue"
+          text="Continue"
           onClick={() => console.log("Button clicked")}
           isDisabled={false}
-          />
-        </div>
-</div>
+        />
+      </div>
+    </div>
   );
-}
+    }
